342f886368bccd89ca1432d654e0439f2c66b450
Paul Portier <paulportier@gmail.com>
2023-08-04 19:16:07 +0200

    add service-worker file (+ companion launcher)

---
+++ app/views/service_worker/service_worker.js
@@ -0,0 +1,17 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
+
+function onInstall(event) {
+  console.log('[Serviceworker]', "Installing!", event);
+}
+
+function onActivate(event) {
+  console.log('[Serviceworker]', "Activating!", event);
+}
+
+function onFetch(event) {
+  console.log('[Serviceworker]', "Fetching!", event);
+}
+
+self.addEventListener('install', onInstall);
+self.addEventListener('activate', onActivate);
+self.addEventListener('fetch', onFetch);

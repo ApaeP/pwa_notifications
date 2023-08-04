c04087acab8d5c9e1d6b9538592b8fe056a0f944
Paul Portier <paulportier@gmail.com>
2023-08-04 19:17:36 +0200

    add routes to render service worker and manifest

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # PWA notifications setup
+  get "/service-worker.js", to: "service_worker#service_worker"
+  get "/manifest.json",     to: "service_worker#manifest"
 end

36875a433daab35e1f7f4b1ff67a9f81ba96e6d1
Paul Portier <paulportier@gmail.com>
2023-08-06 18:43:25 +0200

    style front and send notify button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+      @subscriptions = current_user.subscriptions
+      @subscribed = @subscriptions.any? {  |subscription| subscription.device_id == cookies[:DeviceId] }
+    end
+  end
+
+  def send_test_push_notification
+    PushNotificationService.send(current_user, "PagesController::send_test_push_notification #{Time.zone.now}")
+    render json: { status: 'ok' }, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get "/service-worker.js" => "service_worker#service_worker"
   get "/manifest.json" => "service_worker#manifest"
+  post "/send_test_push_notification", to: "pages#send_test_push_notification", as: :send_test_push_notification
   resources :subscriptions, only: %i[create] do
     collection do
       delete :destroy
